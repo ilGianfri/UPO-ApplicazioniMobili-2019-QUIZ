306c684033d8a245ae631c238ee807720b300022
Alessandro Spisso <20025270@studenti.uniupo.it>
2020-05-20 16:41:54 +0200

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Alessandro Spisso on 20/05/2020.
+//  Copyright © 2020 Alessandro Spisso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
